d07994ccb343cb24661fcd6c90f037440104807c
Andydiantu <Andydiantu233@outlook.com>
2025-08-02 16:14:14 +0100

    added new curriculum learning with keep track of the best running mean

--- src/training_monitor.py
+++ src/training_monitor.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from collections import deque
 
 class TrainingMonitor:
-    def __init__(self, patience, num_timestep_groups, k=5):
+    def __init__(self, patience, num_timestep_groups, k=10):
         self.patience = patience
         self.k = k  # Number of steps to track for running mean
         self.recent_losses = deque(maxlen=k)  # Circular buffer for past k losses
+        self.best_running_mean = float('inf')
         self.running_mean = float('inf')
         self.counter = 0
         self.num_timestep_groups = num_timestep_groups
@@ -24,7 +25,8 @@ class TrainingMonitor:
             self.running_mean = sum(self.recent_losses) / len(self.recent_losses)
             
             # Compare current loss to running mean
-            if loss < self.running_mean:
+            if self.running_mean < self.best_running_mean:
+                self.best_running_mean = self.running_mean
                 self.counter = 0
             else:
                 self.counter += 1
