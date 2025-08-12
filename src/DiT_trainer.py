fixed bug of mismatch devices in FID calculation

src/DiT_trainer.py
@@ -478,7 +478,7 @@ class DiTTrainer:
                     or epoch == self.config.num_epochs - 1
                 ):
                     # Create pipeline with memory optimizations with autocast
-                    with torch.amp.autocast(device_type="cuda", enabled=True):
+                    with torch.amp.autocast(device_type="cuda", enabled=False):
                         # Store original model parameters before applying EMA weights
                         original_model_state = {k: v.cpu().clone() for k, v in model.state_dict().items()}
                         
@@ -599,7 +599,6 @@ class DiTTrainer:
         image_grid.save(f"{test_dir}/{epoch:04d}.png")
 
     def evaluate_fid(self, config, pipeline, num_samples = 5000 ):
-        self.ema_model.copy_to(self.model.parameters())
         fid_score = self.eval.compute_metrics(pipeline, num_samples)
         print(f"FID Score: {fid_score} \n")
         del pipeline

src/eval.py
@@ -21,17 +21,21 @@ class Eval:
 
     def setup_metrics(self, real_features_path):
         self.fid = FrechetInceptionDistance(feature=2048, normalize=True, reset_real_features=False)
-        
+        self.fid.set_dtype(torch.float64)
         # Move FID metric to GPU if available
         if torch.cuda.is_available():
             self.fid = self.fid.cuda()
+            self.fid.set_dtype(torch.float64)
         
         real_features_path = Path(__file__).parent.parent / real_features_path
 
         if real_features_path.exists():
-            self.fid = torch.load(real_features_path, weights_only=False)
+            self.fid = torch.load(real_features_path, weights_only=False, map_location='cuda' if torch.cuda.is_available() else 'cpu')
             print(self.fid.real_features_num_samples)
             print("Real features loaded")
+            self.fid = self.fid.cuda()
+            self.fid.set_dtype(torch.float64)
+
         else:
             print(self.val_dataloader)
             # Precompute real image features